6e43eb133f9d09df68b6fb11a298f11ceb778ee4
hai14894 <hohoanghai14894@gmail.com>
2020-10-14 14:31:41 +1100

    add onClick toggle to all the sidebarMenu

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -10,19 +10,26 @@ import {
   SidebarMenu,
 } from "./SidebarComponents";
 
-const Sidebar = ({toggle, isOpen}) => {
+const Sidebar = ({ toggle, isOpen }) => {
   return (
-      
-    <SidebarContainer toggle={toggle} isOpen={isOpen}>
+    <SidebarContainer isOpen={isOpen}>
       <Icon onClick={toggle}>
         <CloseIcon />
       </Icon>
       <SidebarWrapper>
         <SidebarMenu>
-          <SidebarLink to="about">About</SidebarLink>
-          <SidebarLink to="services">Services</SidebarLink>
-          <SidebarLink to="discover">Discover</SidebarLink>
-          <SidebarLink to="signup">Sign Up</SidebarLink>
+          <SidebarLink to="about" onClick={toggle}>
+            About
+          </SidebarLink>
+          <SidebarLink to="services" onClick={toggle}>
+            Services
+          </SidebarLink>
+          <SidebarLink to="discover" onClick={toggle}>
+            Discover
+          </SidebarLink>
+          <SidebarLink to="signup" onClick={toggle}>
+            Sign Up
+          </SidebarLink>
         </SidebarMenu>
         <SideBtnWrap>
           <SidebarRoute to="/signin"> Sign In</SidebarRoute>
