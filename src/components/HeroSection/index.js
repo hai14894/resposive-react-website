0dace39ad14f62de8e1551df4e1400be7099e1da
hai14894 <hohoanghai14894@gmail.com>
2020-10-15 22:52:28 +1100

    add Hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import{HeroContainer, HeroBg,VideoBg} from './HeroElements'
-import Video from '../../videos/video.mp4'
-const HeroSection = () => {
-    return (
-        <HeroContainer>
-            <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type ='video/mp4'></VideoBg>
-            </HeroBg>
-        </HeroContainer>
-    )
-}
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  Arrow,
+  ArrowHover,
+} from "./HeroElements";
+import {Button} from '../ButtonComponent'
+import Video from "../../videos/video.mp4";
 
-export default HeroSection
+const HeroSection = () => {
+    const [hover,setHover] = useState(false)
+    const onHover = ()=>{
+        setHover(!hover)
+    }
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>This is a sample site</HeroH1>
+        <HeroP>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
+            Get Started {hover ? <Arrow /> : <ArrowHover />}{" "}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
