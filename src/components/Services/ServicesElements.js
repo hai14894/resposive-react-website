b8e29b3ce36ecec6309311b3c86bc4afa6c9c135
hai14894 <hohoanghai14894@gmail.com>
2020-10-20 22:59:06 +1100

    edit ServiceContainer style to fix overfloating elements

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -1,19 +1,14 @@
 import styled from "styled-components";
 
 export const ServicesContainer = styled.div`
-  height: 800px;
+  height: auto;
+  padding-top: 20px;
+  padding-bottom:20px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: #010606;
-
-  @media screen and (max-width: 768px) {
-    height: 1100px;
-  }
-  @media screen and (max-width: 480px) {
-    height: 1300px;
-  }
 `;
 
 export const ServicesWrapper = styled.div`
