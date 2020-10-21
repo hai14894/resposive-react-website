c68599498ed582c2ec8a5e50827a8a98ab91b079
hai14894 <hohoanghai14894@gmail.com>
2020-10-21 16:26:21 +1100

    fix emoji error

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SigninPage = () => {
     return (
         <div>
-            <h1>This page is under construction 👷🏼‍♂️🔨🚧</h1>
+            <h1>This page is under construction</h1> <span role="img" aria-label="S1">👷🏼‍♂️ 🚧 🔨</span>
             <h2>Please come back later</h2>
         </div>
     )
